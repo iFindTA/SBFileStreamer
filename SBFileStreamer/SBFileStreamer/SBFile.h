e028acc11864c00c21f21f1f2dbd99a7b0fd7e04
hujiaju <nanhujiaju@gmail.com>
2018-08-21 15:34:26 +0800

    add new file

---
+++ SBFileStreamer/SBFileStreamer/SBFile.h
@@ -0,0 +1,13 @@
+//
+//  SBFile.h
+//  SBFileStreamer
+//
+//  Created by nanhu on 2018/8/21.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBFile : NSObject
+
+@end
---
+++ SBFileStreamer/SBFileStreamer/SBFile.m
@@ -0,0 +1,13 @@
+//
+//  SBFile.m
+//  SBFileStreamer
+//
+//  Created by nanhu on 2018/8/21.
+//  Copyright © 2018年 nanhu. All rights reserved.
+//
+
+#import "SBFile.h"
+
+@implementation SBFile
+
+@end
